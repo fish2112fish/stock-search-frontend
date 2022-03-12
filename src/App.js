75bb8e35c31ebb286021d591518c68045d6c1d0a
Fisher <fish2112fish@gmail.com>
2022-03-12 21:34:00 +0200

    Added MUI Theme to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+	palette: {
+		type: 'light',
+		primary: {
+			main: '#03004a',
+		},
+		secondary: {
+			main: '#d85400',
+		},
+	},
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<div>New App</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
