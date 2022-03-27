3ef255291d7e6c9ec787407bb5203eff7c5f5aeb
Fisher <fish2112fish@gmail.com>
2022-03-27 13:21:41 +0300

    Fixed again componenet name on Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import HomeSearch from '../components/search/HomeSearch';
 
 const Home = () => {
 	return (
-		<ContentCointainer>
+		<ContentContainer>
 			<img
 				alt="Homepage Logo"
 				src={logo}
@@ -20,7 +20,7 @@ const Home = () => {
 
         <HomeSearch />
 
-		</ContentCointainer>
+		</ContentContainer>
 	);
 
 };
